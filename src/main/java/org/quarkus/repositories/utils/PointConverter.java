2fd3cd94a034c3b95f5d349689dfaef36ad4bfe1
Tom Alexander <tom@saudeagora.com.vc>
2025-01-25 17:42:35 -0300

    FIX - Melhorias no conversor de coordenadas/String WKT

--- src/main/java/org/quarkus/repositories/utils/PointConverter.java
+++ src/main/java/org/quarkus/repositories/utils/PointConverter.java
@@ -4,34 +4,39 @@ import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 import org.locationtech.jts.geom.Point;
 import org.locationtech.jts.io.ParseException;
+import org.locationtech.jts.io.WKBReader;
 import org.locationtech.jts.io.WKTReader;
 import org.locationtech.jts.io.WKTWriter;
 
 @Converter(autoApply = true)
 public class PointConverter implements AttributeConverter<Point, String> {
 
-  private static final WKTWriter writer = new WKTWriter();
   private static final WKTReader reader = new WKTReader();
+  private static final WKTWriter writer = new WKTWriter();
 
   @Override
   public String convertToDatabaseColumn(Point point) {
     if (point == null) {
       return null;
     }
-
     return writer.write(point);
   }
 
   @Override
   public Point convertToEntityAttribute(String wkt) {
-    if (wkt == null || wkt.isEmpty()) {
+    if (wkt == null || wkt.trim().isEmpty()) {
       return null;
     }
 
     try {
       return (Point) reader.read(wkt);
-    } catch (ParseException error) {
-      throw new RuntimeException("Falha ao converter WKT para Point!", error);
+    } catch (ParseException e) {
+      try {
+        WKBReader wkbReader = new WKBReader();
+        return (Point) wkbReader.read(WKBReader.hexToBytes(wkt));
+      } catch (Exception error) {
+        throw new RuntimeException("Falha ao converter WKT/WKB para Point. WKT/WKB inválido: " + wkt, error);
+      }
     }
   }
 }
