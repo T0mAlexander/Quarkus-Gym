793e7d8d75dd7787bc5a98ce75520108c9f9c151
Tom Alexander <tom@saudeagora.com.vc>
2025-01-25 17:40:28 -0300

    FEAT - Record de encapsulamento de coordenadas

--- src/main/java/org/quarkus/algorithms/Coordinates.java
+++ src/main/java/org/quarkus/algorithms/Coordinates.java
@@ -1,3 +1,9 @@
 package org.quarkus.algorithms;
 
-public record Coordinates(double latitude, double longitude) {}
+import org.locationtech.jts.geom.Point;
+
+public record Coordinates(double latitude, double longitude) {
+  public Coordinates(Point location) {
+    this(location.getY(), location.getX());
+  }
+}
