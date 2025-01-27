ce186086bc913970053b7bff908dcda6eb60f27a
Tom Alexander <tom@saudeagora.com.vc>
2025-01-27 12:30:15 -0300

    FEAT - Repositório de check-ins

---
+++ src/main/java/org/quarkus/repositories/CheckInRepository.java
@@ -0,0 +1,16 @@
+package org.quarkus.repositories;
+
+import io.smallrye.mutiny.Uni;
+import org.quarkus.models.CheckIn;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public interface CheckInRepository {
+  Uni<List<CheckIn>> userHistoric(UUID userId, int page);
+  Uni<CheckIn> findPreviousCheckIn(UUID userId, LocalDateTime date);
+  Uni<Integer> checkInsCount(String userId);
+  Uni<CheckIn> findCheckIn(String userId);
+  Uni<CheckIn> create(CheckIn checkIn);
+}
