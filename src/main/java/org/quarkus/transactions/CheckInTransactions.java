53f7ac64931e086eb3994d913565c2dc2f0c53cc
Tom Alexander <tom@saudeagora.com.vc>
2025-01-27 12:34:40 -0300

    FEAT - Transações de check-in

---
+++ src/main/java/org/quarkus/transactions/CheckInTransactions.java
@@ -0,0 +1,40 @@
+package org.quarkus.transactions;
+
+import io.quarkus.hibernate.reactive.panache.PanacheRepository;
+import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.quarkus.models.CheckIn;
+import org.quarkus.repositories.CheckInRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@ApplicationScoped
+public class CheckInTransactions implements PanacheRepository<CheckIn>, CheckInRepository {
+
+  @Override
+  public Uni<List<CheckIn>> userHistoric(UUID userId, int page) {
+    return null;
+  }
+
+  @Override
+  public Uni<CheckIn> findPreviousCheckIn(UUID userId, LocalDateTime date) {
+    return find("userId = ?1 AND DATE(creationDate) = DATE(?2)", userId, date).firstResult();
+  }
+
+  @Override
+  public Uni<Integer> checkInsCount(String userId) {
+    return null;
+  }
+
+  @Override
+  public Uni<CheckIn> findCheckIn(String userId) {
+    return null;
+  }
+
+  @Override
+  public Uni<CheckIn> create(CheckIn checkIn) {
+    return persist(checkIn);
+  }
+}
