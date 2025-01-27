bd9a1527502e6d436102df3ca559384b604fea24
Tom Alexander <tom@saudeagora.com.vc>
2025-01-27 12:30:37 -0300

    FEAT - Super método de busca por ID

--- src/main/java/org/quarkus/repositories/GymRepository.java
+++ src/main/java/org/quarkus/repositories/GymRepository.java
@@ -3,10 +3,12 @@ package org.quarkus.repositories;
 import io.smallrye.mutiny.Uni;
 import org.quarkus.models.Gym;
 import java.util.List;
+import java.util.UUID;
 
 public interface GymRepository {
   Uni<Gym> findByEmail(String email);
   Uni<List<Gym>> searchGyms(String query, int page);
   Uni<List<Gym>> findCloseGyms(Double latitude, Double longitude);
+  Uni<Gym> findById(UUID id);
   Uni<Gym> create(Gym gym);
 }
