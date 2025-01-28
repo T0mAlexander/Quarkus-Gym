9c6a13b18d5ee1a4c169e83ca75d56eef37a767f
Tom Alexander <tom@saudeagora.com.vc>
2025-01-28 16:51:29 -0300

    FEAT - Factory de histórico de check-ins

--- src/main/java/org/quarkus/factories/CheckInFactory.java
+++ src/main/java/org/quarkus/factories/CheckInFactory.java
@@ -1,6 +1,7 @@
 package org.quarkus.factories;
 
 import org.quarkus.services.checkin.CheckInCreationService;
+import org.quarkus.services.checkin.CheckInHistoryService;
 import org.quarkus.transactions.CheckInTransactions;
 import org.quarkus.transactions.GymTransactions;
 
@@ -8,5 +9,9 @@ import org.quarkus.transactions.GymTransactions;
 public class CheckInFactory {
   public CheckInCreationService createService(CheckInTransactions checkIn, GymTransactions gym) {
     return new CheckInCreationService(checkIn, gym);
+  }
+
+  public CheckInHistoryService historyService(CheckInTransactions userHistory) {
+    return new CheckInHistoryService(userHistory);
   }
 }
