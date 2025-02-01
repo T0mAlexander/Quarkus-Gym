27f072619cd018f169bab521fce209c2bf3e15b0
Tom Alexander <tom@saudeagora.com.vc>
2025-02-01 17:50:21 -0300

    FEAT - Criação de submétodos utilitários

--- src/main/java/org/quarkus/services/user/TokenService.java
+++ src/main/java/org/quarkus/services/user/TokenService.java
@@ -7,6 +7,7 @@ import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.quarkus.models.User;
+import org.quarkus.utils.user.Role;
 
 import java.time.Duration;
 import java.util.UUID;
@@ -25,15 +26,34 @@ public class TokenService {
    * Gera um token JWT para o usuário fornecido.
    *
    * @param user O usuário para o qual o token será emitido.
-   * @return O token JWT gerado.
+   * @return Retorna o token de autenticação nos cookies.
    */
   public Uni<String> generateToken(User user) {
     return Uni.createFrom().item(() ->
       Jwt.upn(user.getId().toString())
         .claim("id", user.getId().toString())
+        .claim("role", user.getRole().toString())
         .expiresIn(Duration.ofMinutes(15))
         .sign()
     );
+  }
+
+  public Uni<String> renewToken(String token) {
+    return validateToken(token).onItem().transformToUni(userId -> {
+      if (userId == null) {
+        return Uni.createFrom().failure(
+          new IllegalArgumentException("Token inválido ou expirado!")
+        );
+      }
+
+      return checkRole(token).onItem().transformToUni(role -> {
+        User user = new User();
+        user.setId(userId);
+        user.setRole(role);
+
+        return generateToken(user);
+      });
+    });
   }
 
   public Uni<UUID> validateToken(String token) {
@@ -43,6 +63,27 @@ public class TokenService {
         String userId = jwt.getClaim("id");
 
         return UUID.fromString(userId);
+      } catch (ParseException | IllegalArgumentException error) {
+        return null;
+      }
+    });
+  }
+
+  public Uni<Void> revokeToken() {
+    return Uni.createFrom().voidItem();
+  }
+
+  public Uni<Role> checkRole(String token) {
+    return Uni.createFrom().item(() -> {
+      try {
+        var jwt = service.parse(token);
+        String role = jwt.getClaim("role");
+
+        if (role == null) {
+          throw new IllegalArgumentException("O cargo está vazio!");
+        }
+
+        return Role.valueOf(role);
       } catch (ParseException | IllegalArgumentException error) {
         return null;
       }
